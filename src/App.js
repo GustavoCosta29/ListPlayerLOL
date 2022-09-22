eaaf883ac9581beb792504c3aff6452a293fabb3
Gustavo Costa <gucostapt@gmail.com>
2022-09-22 11:19:55 +0100

    adicionando Banner principal

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import "./App.css";
+import Banner from "./componentes/Banner";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Banner />
+    </div>
+  );
 }
 
 export default App;
